5581a29914263172a66cdd3cc8786210b46cc9bf
wilsmithyef <wilsmithyef@gmail.com>
2024-02-04 12:37:31 -0500

    end of user component, a problem in style index with normalize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import Profile from './Profile/Profile';
+import user from '../utils/user.json';
+const { username, tag, location, avatar, stats } = user;
+
 export const App = () => {
   return (
     <div
@@ -7,10 +11,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
     </div>
   );
 };
